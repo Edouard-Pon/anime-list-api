68eb04d483b642364220c90e9d206f05e29f8039
Edouard-Pon <91964969+Edouard-Pon@users.noreply.github.com>
2022-04-21 01:19:53 +0200

    Finished Character Routes

--- models/character.js
+++ models/character.js
@@ -1,10 +1,23 @@
 const mongoose = require('mongoose')
+const Anime = require('./anime')
 
 const characterSchema = new mongoose.Schema({
     name: {
         type: String,
         required: true
     }
+})
+
+characterSchema.pre('remove', function (next) {
+    Anime.find({ character: this.id }, (err, animes) => {
+        if (err) {
+            next(err)
+        } else if (animes.length > 0) {
+            next(new Error('This character has anime still'))
+        } else {
+            next()
+        }
+    })
 })
 
 module.exports = mongoose.model('Character', characterSchema)
--- routes/characters.js
+++ routes/characters.js
@@ -1,6 +1,7 @@
 const express = require('express')
 const router = express.Router()
 const Character = require('../models/character')
+const Anime = require('../models/anime')
 
 // All Characters Route
 router.get('/', async (req, res) => {
@@ -31,13 +32,68 @@ router.post('/', async (req, res) => {
     })
     try {
         const newCharacter = await character.save()
-        // res.redirected(`characters/${newCharacter.id}`)
-        res.redirect(`characters`)
+        res.redirect(`characters/${newCharacter.id}`)
     } catch {
         res.render('characters/new', {
             character: character,
             errorMessage: 'Error creating Character'
         })
+    }
+})
+
+router.get('/:id', async (req, res) => {
+    try {
+        const character = await Character.findById(req.params.id)
+        const animes = await  Anime.find({ character: character.id }).limit(12).exec()
+        res.render('characters/show', {
+            character: character,
+            animesByCharacter: animes
+        })
+    } catch {
+        res.redirect('/')
+    }
+})
+
+router.get('/:id/edit', async (req, res) => {
+    try {
+        const character = await Character.findById(req.params.id)
+        res.render('characters/edit', { character: character })
+    } catch {
+        res.redirect('/characters')
+    }
+})
+
+router.put('/:id', async (req, res) => {
+    let character
+    try {
+        character = await Character.findById(req.params.id)
+        character.name = req.body.name
+        await character.save()
+        res.redirect(`/characters/${character.id}`)
+    } catch {
+        if (character == null) {
+            res.redirect('/')
+        } else {
+            res.render('characters/edit', {
+                character: character,
+                errorMessage: 'Error updating Character'
+            })
+        }
+    }
+})
+
+router.delete('/:id', async (req, res) => {
+    let character
+    try {
+        character = await Character.findById(req.params.id)
+        await character.remove()
+        res.redirect('/characters')
+    } catch {
+        if (character == null) {
+            res.redirect('/')
+        } else {
+            res.redirect(`/characters/${character.id}`)
+        }
     }
 })
 
