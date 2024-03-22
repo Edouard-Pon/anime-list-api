862500470f030373f18b43a04cf9d2b3b2beca5c
Edouard-Pon <91964969+Edouard-Pon@users.noreply.github.com>
2024-03-22 23:25:54 +0100

    Update characters route

--- routes/characters.js
+++ routes/characters.js
@@ -4,117 +4,131 @@ const Character = require('../models/character')
 const Anime = require('../models/anime')
 const imageMimeTypes = ['image/jpeg', 'image/png', 'image/gif']
 
-// All Characters Route
+
+// Get all characters /characters TODO - Add pagination
 router.get('/', async (req, res) => {
-    let searchOptions = {}
-    if (req.query.name != null && req.query.name !== '') {
-        searchOptions.name =  new RegExp(req.query.name, 'i')
-    }
     try {
-        const characters =  await Character.find(searchOptions)
-        res.render('characters/index', {
+        let characters =  await Character.find()
+        characters = characters.map(character => {
+            const { image, imageType, ...characterWithoutImage } = character._doc
+            return { ...characterWithoutImage, imagePath: character.imagePath }
+        })
+        res.json({
             characters: characters,
-            searchOptions: req.query
         })
     } catch {
-        res.redirect('/')
+        res.status(500).json({ message: 'An error occurred while retrieving the data.' })
     }
 })
 
-// New Character Route
-router.get('/new', async (req, res) => {
-    const animes = await Anime.find({})
-    res.render('characters/new', { animes: animes, character: new Character() })
-})
-
-// Create Character Route
-router.post('/', async (req, res) => {
-    const character = new Character({
-        name: req.body.name,
-        surname: req.body.surname,
-        age: req.body.age,
-        anime: req.body.anime,
-        description: req.body.description
-    })
-    try {
-        saveImage(character, req.body.image)
-        const newCharacter = await character.save()
-        res.redirect(`characters/${newCharacter.id}`)
-    } catch {
-        const animes = await Anime.find({})
-        res.render('characters/new', {
-            character: character,
-            animes: animes,
-            errorMessage: 'Error creating Character'
-        })
-    }
-})
-
+// Get Character by ID /characters/:id
 router.get('/:id', async (req, res) => {
     try {
-        const character = await Character.findById(req.params.id)
-        const animes = await  Anime.find({ _id: character.anime }).exec()
-        res.render('characters/show', {
+        let character = await Character.findById(req.params.id)
+        let animes = await  Anime.find({ _id: character.anime }).exec()
+        const { image, imageType, ...characterWithoutImage } = character._doc
+        character = { ...characterWithoutImage, imagePath: character.imagePath }
+        animes = animes.map(anime => {
+            const { coverImage, coverImageType, ...animeWithoutCoverImage } = anime._doc
+            return { ...animeWithoutCoverImage, coverImagePath: anime.coverImagePath }
+        })
+        res.json({
             character: character,
             animes: animes
         })
     } catch {
-        res.redirect('/')
+        res.status(500).json({ message: 'An error occurred while retrieving the data.' })
     }
 })
 
-router.get('/:id/edit', async (req, res) => {
+// Search Characters /characters/search
+router.post('/search', async (req, res) => {
+    let searchOptions = {}
+    if (req.body.name != null && req.body.name !== '') {
+        searchOptions.name = new RegExp(req.body.name, 'i')
+    }
     try {
-        const character = await Character.findById(req.params.id)
-        const animes = await Anime.find({})
-        res.render('characters/edit', { animes: animes, character: character })
+        let characters =  await Character.find(searchOptions)
+        characters = characters.map(character => {
+            const { image, imageType, ...characterWithoutImage } = character._doc
+            return { ...characterWithoutImage, imagePath: character.imagePath }
+        })
+        res.json({
+            characters: characters,
+            searchOptions: req.body
+        })
     } catch {
-        res.redirect('/characters')
+        res.status(500).json({ message: 'An error occurred while retrieving the data.' })
     }
 })
 
-router.put('/:id', async (req, res) => {
-    let character
-    const animes = await Anime.find({})
-    try {
-        character = await Character.findById(req.params.id)
-        character.name = req.body.name
-        character.surname = req.body.surname
-        character.age = req.body.age
-        character.description = req.body.description
-        character.anime = req.body.anime
-        if (req.body.image != null && req.body.image !== '') {
-            saveImage(character, req.body.image)
-        }
-        await character.save()
-        res.redirect(`/characters/${character.id}`)
-    } catch {
-        if (character == null) {
-            res.redirect('/')
-        } else {
-            res.render('characters/edit', {
-                character: character,
-                animes: animes,
-                errorMessage: 'Error updating Character'
-            })
-        }
-    }
-})
+// Create Character Route TODO - Create character in database
+// router.post('/', async (req, res) => {
+//     const character = new Character({
+//         name: req.body.name,
+//         surname: req.body.surname,
+//         age: req.body.age,
+//         anime: req.body.anime,
+//         description: req.body.description
+//     })
+//     try {
+//         saveImage(character, req.body.image)
+//         const newCharacter = await character.save()
+//         res.redirect(`characters/${newCharacter.id}`)
+//     } catch {
+//         const animes = await Anime.find({})
+//         res.render('characters/new', {
+//             character: character,
+//             animes: animes,
+//             errorMessage: 'Error creating Character'
+//         })
+//     }
+// })
 
-router.delete('/:id', async (req, res) => {
-    let character
-    try {
-        character = await Character.findById(req.params.id)
-        await character.remove()
-        res.redirect('/characters')
-    } catch {
-        if (character == null) {
-            res.redirect('/')
-        } else {
-            res.redirect(`/characters/${character.id}`)
-        }
-    }
-})
+// Update Character Route TODO - Update character in database
+// router.put('/:id', async (req, res) => {
+//     let character
+//     const animes = await Anime.find({})
+//     try {
+//         character = await Character.findById(req.params.id)
+//         character.name = req.body.name
+//         character.surname = req.body.surname
+//         character.age = req.body.age
+//         character.description = req.body.description
+//         character.anime = req.body.anime
+//         if (req.body.image != null && req.body.image !== '') {
+//             saveImage(character, req.body.image)
+//         }
+//         await character.save()
+//         res.redirect(`/characters/${character.id}`)
+//     } catch {
+//         if (character == null) {
+//             res.redirect('/')
+//         } else {
+//             res.render('characters/edit', {
+//                 character: character,
+//                 animes: animes,
+//                 errorMessage: 'Error updating Character'
+//             })
+//         }
+//     }
+// })
+
+// Delete Character Route TODO - Delete character in database
+// router.delete('/:id', async (req, res) => {
+//     let character
+//     try {
+//         character = await Character.findById(req.params.id)
+//         await character.remove()
+//         res.redirect('/characters')
+//     } catch {
+//         if (character == null) {
+//             res.redirect('/')
+//         } else {
+//             res.redirect(`/characters/${character.id}`)
+//         }
+//     }
+// })
 
 function saveImage(character, imageEncoded) {
     if (imageEncoded == null) return
