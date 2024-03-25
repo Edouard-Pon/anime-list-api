c11049852fe4397ad32d3d374398b928edb8c026
Edouard-Pon <91964969+Edouard-Pon@users.noreply.github.com>
2024-03-25 02:06:51 +0100

    Add user model and router
    Implement register and login routes

---
+++ routes/user.js
@@ -0,0 +1,71 @@
+if (process.env.NODE_ENV !== 'production') {
+    require('dotenv').config()
+}
+const secretKey = process.env.SECRET_KEY
+const express = require('express')
+const router = express.Router()
+const User = require('../models/user')
+const jwt = require('jsonwebtoken')
+const { body, validationResult } = require('express-validator')
+
+
+router.post('/login',
+    body('username').trim().escape(),
+    body('password').trim().escape(),
+    async (req, res) => {
+        const errors = validationResult(req)
+        if (!errors.isEmpty()) {
+            return res.status(400).json({ errors: errors.array() })
+        }
+
+        const { username, password } = req.body
+
+        const user = await User.findOne({ username })
+        if (!user) {
+            return res.status(401).json({ message: 'Authentication failed.' })
+        }
+
+        user.comparePassword(password, (err, isMatch) => {
+            if (err) {
+                return res.status(500).json({ message: 'Error authenticating user.' })
+            }
+            if (!isMatch) {
+                return res.status(401).json({ message: 'Authentication failed.' })
+            }
+
+            const token = jwt.sign(user.toJSON(), secretKey, {
+                expiresIn: 10080
+            })
+            res.status(200).json({ success: true, token: 'JWT ' + token })
+        })
+    }
+)
+
+router.post('/register',
+    body('username').trim().escape(),
+    body('password').trim().escape(),
+    async (req, res) => {
+        const errors = validationResult(req)
+        if (!errors.isEmpty()) {
+            return res.status(400).json({ errors: errors.array() })
+        }
+
+        const { username, password } = req.body
+
+        const existingUser = await User.findOne({ username })
+        if (existingUser) {
+            return res.status(400).json({ message: 'Username already exists.' })
+        }
+
+        const user = new User({ username, password })
+
+        try {
+            await user.save()
+            res.status(200).json({message: 'User registered successfully.'})
+        } catch (err) {
+            res.status(500).json({message: 'Error registering new user.'})
+        }
+    }
+)
+
+module.exports = router
--- server.js
+++ server.js
@@ -6,9 +6,9 @@ const express = require('express')
 const app = express()
 const methodOverride = require('method-override')
 
-const characterRouter = require('./routes/characters')
-const animeRouter = require('./routes/animes')
-const profileRouter = require('./routes/profile')
+const characterRouter = require('./routes/character')
+const animeRouter = require('./routes/anime')
+const userRouter = require('./routes/user')
 
 app.use(express.json())
 app.use(express.urlencoded({ extended: false }))
@@ -19,6 +19,6 @@ mongoose.connect(process.env.DATABASE_URL).then(() => console.log('Connected to 
 
 app.use('/characters', characterRouter)
 app.use('/anime', animeRouter)
-app.use('/profile', profileRouter)
+app.use('/user', userRouter)
 
 app.listen(process.env.PORT || 3000)
